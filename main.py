342f72482ae5c8d0bb9f3d63d52f4ba2410db92d
John Till <johndtill@gmail.com>
2021-07-07 17:37:32 -0500

    Initial commit

---
+++ main.py
@@ -0,0 +1,154 @@
+import sys
+from PyQt5.QtWidgets import *
+from PyQt5.QtGui import QFont
+
+
+ascii_art = [
+    "       |>\n"
+    "       |   \\O/\n"
+    "~^~^~~ | ~~ | ~~^~~",
+
+    "     ____--\n"
+    "   _/  /   O\n"
+    " _/   /  -/-\n"
+    "/    /    |\\\n"
+    "    | ^----- ~~~~~~~",
+
+    "    /-        O\n"
+    "   /\\       -/-\n"
+    "  /  \\       |\\\n"
+    "~/    \\~ ^----- ~~~~~~~",
+
+    "             O\n"
+    "           -/-\n"
+    "  /\\        |\\\n"
+    "~/  \\~~ ^----- ~~~~~~~",
+
+    "        O\n"
+    "      -/-\n"
+    "       |\\\n"
+    "~~ ^----- ~~~~~~~"
+]
+
+ascii_win = "          \\O_\n" \
+            "          /\n" \
+            "         |\\\n" \
+            "     ^-----\n" \
+            "   ------\n" \
+            " _/  /\n" \
+            "/   /\n" \
+            "    | ~~~~~~~"
+
+
+class DlgMain(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("Hang-ten man")
+        self.resize(200, 200)
+        self.mainLayout = QVBoxLayout()
+
+        self.word = "Harmony"
+        self.guessedLetters = []
+        self.lives = 3
+
+        font = QFont("unexistent")
+        font.setStyleHint(QFont.Monospace)
+
+        self.pictorialEdit = QPlainTextEdit(self)
+        self.pictorialEdit.setReadOnly(True)
+        self.pictorialEdit.setPlainText(ascii_art[-1])
+        self.pictorialEdit.setFont(font)
+        self.pictorialEdit.setLineWrapMode(QPlainTextEdit.NoWrap)
+        self.mainLayout.addWidget(self.pictorialEdit)
+
+        self.wordLabel = QLabel()
+        self.update_display_word()
+        self.mainLayout.addWidget(self.wordLabel)
+
+        self.guessLabel = QLabel()
+        self.update_display_guesses()
+        self.mainLayout.addWidget(self.guessLabel)
+
+        self.hintLabel = QLabel("Go ahead- guess a letter!")
+        self.mainLayout.addWidget(self.hintLabel)
+
+        self.inputLayout = QHBoxLayout()
+        self.inputLayout.addWidget(QLabel("Guess: "))
+        self.guessEdit = QLineEdit(self)
+        self.guessEdit.setMaxLength(1)
+        self.guessEdit.returnPressed.connect(self.on_submit)
+        self.inputLayout.addWidget(self.guessEdit)
+        self.btn = QPushButton("Submit")
+        self.btn.pressed.connect(self.on_submit)
+        self.inputLayout.addWidget(self.btn)
+        self.mainLayout.addLayout(self.inputLayout)
+
+        self.setCentralWidget(QWidget(self))
+        self.centralWidget().setLayout(self.mainLayout)
+
+    def update_display_word(self):
+        hidden_word = ""
+        for ch in self.word:
+            if ch.lower() in self.guessedLetters:
+                hidden_word += ch
+            else:
+                hidden_word += '*'
+        self.wordLabel.setText(hidden_word)
+
+    def update_display_guesses(self):
+        self.guessLabel.setText("{} guesses remaining".format(self.lives))
+        if(self.lives < len(ascii_art)):
+            self.pictorialEdit.setPlainText(ascii_art[self.lives])
+        else:
+            self.pictorialEdit.setPlainText(ascii_art[-1])
+
+    def on_submit(self):
+        if self.guessEdit.isEnabled():
+            self.on_guess()
+        else:
+            self.reset_game()
+
+    def on_guess(self):
+        guess = self.guessEdit.text().lower()
+        if not guess:
+            self.hintLabel.setText("Enter a letter in the box, bro!")
+        elif not guess.isalpha():
+            self.hintLabel.setText("Letters only, dude!")
+        elif guess in self.guessedLetters:
+            self.hintLabel.setText("You already guessed '{}', dude!".format(guess))
+        elif guess in self.word.lower():
+            self.hintLabel.setText("'{}' is part of the word!".format(guess))
+            self.guessedLetters.append(guess)
+            self.update_display_word()
+            if self.wordLabel.text() == self.word:
+                self.hintLabel.setText("Radical! You win!")
+                self.pictorialEdit.setPlainText(ascii_win)
+                self.btn.setText("New game")
+                self.guessEdit.setEnabled(False)
+        else:
+            self.hintLabel.setText("'{}' is not present.".format(guess))
+            self.guessedLetters.append(guess)
+            self.lives -= 1
+            self.update_display_guesses()
+            if not self.lives:
+                self.hintLabel.setText("Wipe out!")
+                self.btn.setText("New game")
+                self.guessEdit.setEnabled(False)
+
+    def reset_game(self):
+        self.guessedLetters = []
+        self.lives = 3
+        self.word = "Aardvark"
+        self.update_display_word()
+        self.update_display_guesses()
+        self.btn.setText("Submit")
+        self.hintLabel.setText("Go ahead- guess a letter!")
+        self.guessEdit.clear()
+        self.guessEdit.setEnabled(True)
+
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    dlgMain = DlgMain()
+    dlgMain.show()
+    sys.exit(app.exec_())
